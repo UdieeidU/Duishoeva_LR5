dd5fc37b967e6caae928f8bf4e98417b0cc8600b
UdieeidU <duishoevaaidai@gmail.com>
2025-05-23 21:33:35 +0300

    2

--- Duishoeva_MathTask.h
+++ Duishoeva_MathTask.h
@@ -34,7 +34,7 @@ void EnterDigit(int& varLink, const string& label) {
 
 // вычисление площади прямоугольника
 int CalcRectangleArea(int NumberA, int NumberB) {
-    return NumberA * NumberB;
+    return NumberA * NumberB + 10;
 }
 
 #endif
